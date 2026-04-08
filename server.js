1737187dcab02157cd3cfa1042902e8714952c1e
anirban goswami <agoswami@cineverse.com>
2026-04-08 16:51:16 +0530

    dd

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ let temperature = 0;
 let clients = [];
 
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.get('/api/temp', (req, res) => {
     res.json({ temperature });
 });
