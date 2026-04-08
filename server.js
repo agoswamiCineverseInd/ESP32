f90ba9129040d3ebe9df06d03e91d02ffa22972a
anirban goswami <agoswami@cineverse.com>
2026-04-08 19:13:49 +0530

    c

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// ✅ THIS FIXES YOUR ISSUE
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
