8b1fd2bab003db399d51662af5ee0058e725860b
anirban goswami <agoswami@cineverse.com>
2026-04-08 17:04:31 +0530

    dd

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors=require('cors')
 const app = express();
 
 app.use(express.json());
 app.use(express.static('public'));
+app.use(cors())
 
 let temperature = 0;
 let clients = [];
